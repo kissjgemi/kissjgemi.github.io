6e3d7804e6dbd3de9ccd3b2fd9b7214cf8299edb
kissjgemi <b6sics@b6.hu>
2021-09-02 06:01:31 +0200

    add fitoterápia

---
+++ Fitoterápia.HTM/Mammy Kuckója_files/doDocWrite.js
@@ -0,0 +1,4 @@
+// does docwrite to avoid ie's ActiveX activation bullshit
+function doDocWrite ( something ) {
+	document.write( something );
+}
---
+++ Fitoterápia.HTM/Mammy Kuckója_files/gwloader.js
@@ -0,0 +1,2 @@
+if(!window.goAdverticum)document.write('<sc'+'ript src="http://imgs.adverticum.net/scripts/goAdverticum1.24.js" type="text/javascript"></sc'+'ript>');
+document.write('<sc'+'ript src="http://imgs.adverticum.net/scripts/doDocWrite.js" type="text/javascript"></sc'+'ript>')
